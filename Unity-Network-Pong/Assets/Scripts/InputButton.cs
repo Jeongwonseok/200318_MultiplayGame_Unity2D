a193830ea29e716fe5453d56c639840b320a6c3d
“Jeongwonseok” <jws4281@nate.com>
2020-03-23 00:29:23 +0900

    InputButton
    - InputButton 구현
    - Up & Down 버튼 구현
    - VerticalInput 제공
    (-1 아래 방향 / +1 위 방향)

--- Unity-Network-Pong/Assets/Scripts/InputButton.cs
+++ Unity-Network-Pong/Assets/Scripts/InputButton.cs
@@ -13,28 +13,46 @@ public class InputButton : MonoBehaviour
 
     private State state = State.None;
 
+    // 현재 State에 따라 VerticalInput 값 지정
     private void Update()
     {
-
+        if(state == State.None)
+        {
+            VerticalInput = 0f;
+        }
+        else if(state == State.Up)
+        {
+            VerticalInput = 1f;
+        }
+        else if(state == State.Down)
+        {
+            VerticalInput = -1f;
+        }
     }
 
     public void OnMoveUpButtonPressed()
     {
-
+        state = State.Up;
     }
 
     public void OnMoveUpButtonUp()
     {
-
+        if(state == State.Up)
+        {
+            state = State.None;
+        }
     }
 
     public void OnMoveDownButtonPressed()
     {
-
+        state = State.Down;
     }
 
     public void OnMoveDownButtonUp()
     {
-
+        if(state == State.Down)
+        {
+            state = State.None;
+        }
     }
 }
